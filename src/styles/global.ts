8b919c07bca72dd3674da00d3b389549d3ccdd7f
Anthony2365 <anthonyvinicius124@gmail.com>
2021-12-02 16:37:10 -0300

    Estilização Global do projeto

---
+++ src/styles/global.ts
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import {createGlobalStyle} from 'styled-components'
+
+export default createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0:
+        outilene: 0:
+        box-sizing: border-box;
+
+    }
+
+    boby{
+        background: #F0F0F5;
+    }
+
+    body , imput , button {
+        font: 16px Roboto, sans-serif;
+    }
+
+    #root{
+        max-width: 960px;
+        margin: 0 auto;
+        padding: 40px 20px;
+    }
+
+    button{
+        cursor: pointer;
+    }
+`;
