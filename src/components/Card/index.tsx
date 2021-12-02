b6899d0ea36acef4e6a38956811aef777a3e9b2d
Anthony2365 <anthonyvinicius124@gmail.com>
2021-12-02 16:37:45 -0300

    Configuração de teste

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Card: React.FC = () =>{
+    return(
+        <>
+        <h3>Card</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque mollitia nesciunt itaque possimus architecto laboriosam ex natus, nostrum nulla delectus laborum accusantium, recusandae alias ipsam ipsa officiis sint odit facere.</p>
+        </>
+    );
+}
+
+export default Card;
