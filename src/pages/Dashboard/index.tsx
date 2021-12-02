c5c96b78417b9c45349f6781ceb2b24e95107a50
Anthony2365 <anthonyvinicius124@gmail.com>
2021-12-02 16:38:29 -0300

    Configuração de tela Dashbord

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,52 @@
 import React from "react";
+import {FiChevronRight} from 'react-icons/fi'
+import {Link} from 'react-router-dom'
+
+import {Form, Title, Repositories} from './styles';
 
 const Dashboard: React.FC = () => {
     return(
-        <h3>Pagina Deshboard</h3>
+        <>
+            <Title>Explore repositórios no GitHub</Title>
+            <Form>
+                <input placeholder="Digite o nome do repositório" />
+                <button type="submit">Pesquisar</button>
+            </Form>
+
+            <Repositories>
+                <Link to="#">
+                    <img src="https://avatars.githubusercontent.com/u/83778642?v=4" alt="" />
+                    <div>
+                        <strong>React</strong>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia illum illo nulla explicabo nobis esse, sint iusto ipsum aspernatur, ea, veritatis itaque libero impedit a quidem! Expedita et illum dicta?</p>
+                    </div>
+                    <FiChevronRight />
+                </Link>
+            </Repositories>
+
+            <Repositories>
+                <Link to="#">
+                    <img src="https://avatars.githubusercontent.com/u/83778642?v=4" alt="" />
+                    <div>
+                        <strong>React</strong>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia illum illo nulla explicabo nobis esse, sint iusto ipsum aspernatur, ea, veritatis itaque libero impedit a quidem! Expedita et illum dicta?</p>
+                    </div>
+                    <FiChevronRight />
+                </Link>
+            </Repositories>
+
+            <Repositories>
+                <Link to="#">
+                    <img src="https://avatars.githubusercontent.com/u/83778642?v=4" alt="" />
+                    <div>
+                        <strong>React</strong>
+                        <p>Lorem ipsum dolor , sit amet consectetur adipisicing elit. Quia illum illo nulla explicabo nobis esse, sint iusto ipsum aspernatur, ea, veritatis itaque libero impedit a quidem! Expedita et illum dicta?</p>
+                    </div>
+                    <FiChevronRight />
+                </Link>
+            </Repositories>
+
+        </>
     );
 }
 
